9e66f4c9fabf608b8c041152512b67a604065f72
rainharp <xuetingyu@126.com>
2022-06-14 11:03:03 +0800

    Update ZDDE.m

--- ZDDE.m
+++ ZDDE.m
@@ -73,14 +73,15 @@ classdef ZDDE < handle
                 instance = parameter;
             end
 
-            % 尝试创建独立连接
+            % Try create standalone connection
 
-            % 注意 - 若提示 'Unable to load one or more of the requested types', 
-            % 通常是因为尝试连接 32 位的 Zemax 和 64 位的 Matlab, 或 64 位的 
-            % Matlab 和 32 位的 Zemax。这是由 MATLAB 与 .NET 的交互造成的。当前
-            % 只能通过安装同为 32 位或 64 位的 Zemax 和 MATLAB 解决。
+            % NOTE - if this fails with a message like 'Unable to load one or more of
+            % the requested types', it is usually caused by try to connect to a 32-bit
+            % version of OpticStudio from a 64-bit version of MATLAB (or vice-versa).
+            % This is an issue with how MATLAB interfaces with .NET, and the only
+            % current workaround is to use 32- or 64-bit versions of both applications.
 
-            %% 以扩展方式连接
+            %% Connect as extension
             if exist('instance', 'var')
                 Mode = 'Extension';
                 TheApplication = TheConnection.ConnectAsExtension(instance);
@@ -90,7 +91,7 @@ classdef ZDDE < handle
                 if ~TheApplication.IsValidLicenseForAPI
                     %TheApplication.CloseApplication();
                     %HandleError('License check failed!');
-                    HandleError('ZDDE.m, License check failed! 请检查实例编号，或确认是否已打开 Zemax 交互扩展等待连接。');
+                    HandleError('ZDDE.m, License check failed! Please check instance id，or check if Zemax is already open to wait for extension connection.');
                     TheApplication = [];
                 end
             end
@@ -158,7 +159,7 @@ classdef ZDDE < handle
         end
             
         function LDE_InsertECollimator(obj, varargin)
-            % 在序列模式中插入带8度面的准直器；；
+            % Insert collimator with tilt surface in sequence mode.
             % InsertCollimator(3.850, 1.800, 'N-SF11', 0.198, 0.5);
             length = varargin{1};
             radius = varargin{2};
@@ -175,41 +176,41 @@ classdef ZDDE < handle
             Surface_3 = obj.LDE.GetSurfaceAt(2);
             Surface_4 = obj.LDE.GetSurfaceAt(3);
 
-            % 设置 d0, 透镜长度
+            % Set d0 and length of c-lens
             Surface_1.Thickness = 0;
             Surface_2.Thickness = d0;
             Surface_3.Thickness = length;
             Surface_4.Thickness = 0;
 
-            % 设置材料
+            % Set material
             Surface_1.Material = 'F_Silica';  
             Surface_2.Material = '';  
             Surface_3.Material = material;  
             Surface_4.Material = '';
 
-            % 设置透镜半径，曲率半径
+            % Set semidiameter and radius of c-lens
             Surface_3.SemiDiameter = semiDiameter;
             Surface_4.SemiDiameter = semiDiameter;
             Surface_4.Radius = -radius;
             
-            % 设置行颜色
+            % Set Row Color
             Surface_1.TypeData.RowColor = ZOSAPI.Common.ZemaxColor.Color13;
             Surface_2.TypeData.RowColor = ZOSAPI.Common.ZemaxColor.Color13;
             Surface_3.TypeData.RowColor = ZOSAPI.Common.ZemaxColor.Color13;
             Surface_4.TypeData.RowColor = ZOSAPI.Common.ZemaxColor.Color13;
             
-            % 设置 8 度面
+            % Set tilt surface (8deg)
             SurfaceType_CB = Surface_2.GetSurfaceTypeSettings(ZOSAPI.Editors.LDE.SurfaceType.Tilted);
             Surface_2.ChangeType(SurfaceType_CB);
             Surface_2.SurfaceData.Y_Tangent = tan(Tilt*pi/180);
             Surface_3.ChangeType(SurfaceType_CB);
             Surface_3.SurfaceData.Y_Tangent = tan(Tilt*pi/180);
 
-            % 设置注释
-            Surface_1.Comment = '准直器';
-            Surface_2.Comment = '准直器';
-            Surface_3.Comment = '准直器';
-            Surface_4.Comment = '准直器';
+            % Set Comment
+            Surface_1.Comment = 'Collimator';
+            Surface_2.Comment = 'Collimator';
+            Surface_3.Comment = 'Collimator';
+            Surface_4.Comment = 'Collimator';
         end
         
         function LDE_InsertRCollimator(obj, varargin)
@@ -227,7 +228,7 @@ classdef ZDDE < handle
             Surface_2 = obj.LDE.GetSurfaceAt(count);
             Surface_3 = obj.LDE.GetSurfaceAt(count+1);
             
-            % 设置透镜长度，曲率，半径, d0
+            % Set lens length, radius, semidiameter and d0
             Surface_1.Thickness = length;
             Surface_1.Radius = radius;
             Surface_1.SemiDiameter = semiDiameter;
@@ -235,27 +236,27 @@ classdef ZDDE < handle
             Surface_3.SemiDiameter = semiDiameter;  
             Surface_2.Thickness = d0;
             
-            % 设置 8 度面
+            % Set tilt surface (8deg)
             SurfaceType_CB = Surface_2.GetSurfaceTypeSettings(ZOSAPI.Editors.LDE.SurfaceType.Tilted);
             Surface_2.ChangeType(SurfaceType_CB);
             Surface_2.SurfaceData.Y_Tangent = tan(Tilt*pi/180);
             Surface_3.ChangeType(SurfaceType_CB);
             Surface_3.SurfaceData.Y_Tangent = tan(Tilt*pi/180);
 
-            % 设置材料
+            % Set material
             Surface_1.Material = material;  
             Surface_2.Material = '';  
             Surface_3.Material = 'F_Silica';  
             
-            % 设置行颜色
+            % Set row color
             Surface_1.TypeData.RowColor = ZOSAPI.Common.ZemaxColor.Color13;
             Surface_2.TypeData.RowColor = ZOSAPI.Common.ZemaxColor.Color13;
             Surface_3.TypeData.RowColor = ZOSAPI.Common.ZemaxColor.Color13;
             
-            % 设置注释
-            Surface_1.Comment = '准直器';
-            Surface_2.Comment = '准直器';
-            Surface_3.Comment = '准直器';
+            % Set comment
+            Surface_1.Comment = 'Collimator';
+            Surface_2.Comment = 'Collimator';
+            Surface_3.Comment = 'Collimator';
         end
         
         function New(obj)
@@ -284,6 +285,8 @@ classdef ZDDE < handle
         end
         
         function IL = getIL(obj)
+            % Calculate insertion loss
+            % last updated: 2022.6.13
             TheSystem = obj.TheApplication.PrimarySystem;
             nsur = TheSystem.LDE.NumberOfSurfaces;
             Efficiency = TheSystem.MFE.GetOperandValue(ZOSAPI.Editors.MFE.MeritOperandType.POPD, nsur, 0, 0, 0, 0, 0, 0, 0);
@@ -298,21 +301,22 @@ classdef ZDDE < handle
         end
         
         function setStop(obj, surfaceID)
-            obj.TheApplication.PrimarySystem.LDE.GetSurfaceAt(surfaceID).TypeData.IsStop = 1;      % 设置面 surfaceID 为光阑
+            obj.TheApplication.PrimarySystem.LDE.GetSurfaceAt(surfaceID).TypeData.IsStop = 1;      % set surfaceID as stop
         end
         
         function setWavelength(obj, wavelength)
+            %setWavelength - set Primary Wavelength
+            % last updated: 2022.6.13
             obj.TheApplication.PrimarySystem.SystemData.Wavelengths.GetWavelength(1).Wavelength = wavelength;
             obj.TheApplication.PrimarySystem.SystemData.Wavelengths.GetWavelength(1).MakePrimary;
         end
         
         function varargout = getMFE(obj,varargin)
-            %getMFE - 获取 OpticStudio Zemax 的评价函数表（值）
+            %getMFE - get merit function table (value)
             %
-            %	作者：Tingyu Xue
-            %	更新日期： 2021.7.24
-            %	函数版本： 1.0
-            %     函数依赖： 无
+            %	Author: Tingyu Xue
+            %	Last updated: 2021.7.24
+            %	Version: 1.0
             %
             %	此 MATLAB 函数 在已建立储存于 TheApplication 全局变量的 ZOSAPI_Application
             %   对象的基础上，计算并返回 Zemax 评价函数表（值）。无输入参数时，计算并返回评
@@ -326,12 +330,12 @@ classdef ZDDE < handle
 
                 TheApplication = obj.TheApplication;
                 if isempty(TheApplication)
-                    disp('未找到 ZOSAPI_Application 对象！');
+                    disp('ZOSAPI_Application object not find.');
                     return;
                 elseif strcmp(class(TheApplication),'ZemaxUI.Common.ViewModels.ZOSAPI_Application')
-                    N = TheApplication.PrimarySystem.MFE.NumberOfOperands;    % 评价函数数量
+                    N = TheApplication.PrimarySystem.MFE.NumberOfOperands;    % operand count
                     TheMFE = TheApplication.PrimarySystem.MFE;
-                    TheMFE.CalculateMeritFunction();                          % 计算评价函数
+                    TheMFE.CalculateMeritFunction();                          % calculate merit function
                     if nargin < 2          
                         for ii = 1: N
                             Type{ii,1} = char(TheApplication.PrimarySystem.MFE.GetOperandAt(ii).RowTypeName);
@@ -339,30 +343,30 @@ classdef ZDDE < handle
                             Weight(ii,1) = TheApplication.PrimarySystem.MFE.GetOperandAt(ii).Weight;
                             Value(ii,1) = TheApplication.PrimarySystem.MFE.GetOperandAt(ii).Value;
                         end
-                        MFETable = table(Type,Target,Weight, Value);   % 返回评价函数表
+                        MFETable = table(Type,Target,Weight, Value);   % returns merit function table
                         varargout{1} = MFETable;
                     elseif nargin == 2
                         id = varargin{1};
                         if length(id) == 1
                             if id==fix(id) && id <= N && id > 0
-                                varargout{1} = TheMFE.GetOperandAt(id).Value; % 返回第id行评价函数的值
+                                varargout{1} = TheMFE.GetOperandAt(id).Value;         % return MFE value at row id
                             else
-                                disp('评价函数序号错误！');
+                                disp('MFE ID error！');
                                 return;
                             end
                         else
                             for i = 1:length(id)
                                 if id(i)==fix(id(i)) && id(i) <= N && id(i) > 0
-                                    varargout{i} = TheMFE.GetOperandAt(id(i)).Value;  % 返回第id(i)行评价函数的值
+                                    varargout{i} = TheMFE.GetOperandAt(id(i)).Value;  % return MFE value at row id(i)
                                 else
-                                    disp('评价函数序号错误！');
+                                    disp('MFE ID error！');
                                     return;
                                 end
                             end
                         end
                     end
                 else
-                    disp('变量类型错误。全局变量 TheApplication 中应保存 ZOSAPI_Application 对象! ');
+                    disp('Variable type error. The ZOSAPI_Application object should be save in global variable TheApplication.');
                     return;
                 end
         end
@@ -372,8 +376,8 @@ classdef ZDDE < handle
         end
         
         function NSCTrace(obj, FileName)
-            % 非序列光线追迹
-            File = obj.TheSystem.SystemFile;               % Zemax 文件路径
+            % NSC Ray Tracing
+            File = obj.TheSystem.SystemFile;              % Zemax File Path
             obj.TheSystem.LoadFile(File, false);
             if ~isempty(obj.TheSystem.Tools.CurrentTool)
                 obj.TheSystem.Tools.CurrentTool.Close();
@@ -411,7 +415,7 @@ classdef ZDDE < handle
         end
         
         function Result = getZRDResult(obj, ZRDFilePath)
-        % 读取 ZRD 文件
+        % Read ZRD Files
             if ~strcmp(ZRDFilePath(2),':')
                 ZRDFilePath = fullfile(pwd, ZRDFilePath);
             end
@@ -423,12 +427,112 @@ classdef ZDDE < handle
             ZRDReader.ZRDFile = ZRDFilePath;
             ZRDReader.RunAndWaitForCompletion();
             if ZRDReader.Succeeded == 0
-                disp('ZRD 文件读取失败!');
+                disp('ZRD File Reading Failed!');
                 disp(ZRDReader.ErrorMessage);
             else
-                disp('ZRD 文件读取成功!');
+                disp('ZRD File Reading Succeed!');
             end
             Result = ZRDReader.GetResults();
+        end
+        
+        function RayTable = getRayTable(obj, ZRDFilePath, varargin)
+        %getRayTable - Read ZRD Files and save ray data to RayTable
+        % last updated: 2022.6.13
+            if nargin == 2
+                filterString = "";
+                option = false;                
+            elseif nargin == 3
+                filterString = varargin{1};
+                option = false;
+            else
+                filterString = varargin{1};
+                option = varargin{2};
+            end
+            if ~strcmp(ZRDFilePath(2),':')
+                ZRDFilePath = fullfile(pwd, ZRDFilePath);
+            end
+            TheSystem = obj.TheSystem;
+            if ~isempty(TheSystem.Tools.CurrentTool)
+                TheSystem.Tools.CurrentTool.Close();
+            end
+            ZRDReader = TheSystem.Tools.OpenRayDatabaseReader();
+            ZRDReader.ZRDFile = ZRDFilePath;
+            ZRDReader.Filter = filterString;
+            ZRDReader.RunAndWaitForCompletion();
+            if ZRDReader.Succeeded == 0
+                disp('ZRD File Reading Failed!');
+                disp(ZRDReader.ErrorMessage);
+            else
+                disp('ZRD File Reading Succeed!');
+            end
+            ZRDResult = ZRDReader.GetResults();
+            [success_NextResult, rayNumber, waveIndex, wlUM, numSegments] = ZRDResult.ReadNextResult();
+            BeamCounter = 1;                                             % Light beam counter
+            RowCounter = 1;                                              % Row counter
+
+            while success_NextResult == 1
+                SegmentCounter = 1;                                      % Segment counter
+                [t_success_NextSegmentFull, t_segmentLevel, t_segmentParent, t_hitObj, t_hitFace, t_insideOf, t_status, ...
+                      t_x, t_y, t_z, t_l, t_m, t_n, t_exr, t_exi, t_eyr, t_eyi, t_ezr, t_ezi, t_intensity, t_pathLength,...
+                      t_xybin, t_lmbin, t_xNorm, t_yNorm, t_zNorm, t_index, t_startingPhase, t_phaseOf, t_phaseAt] = ZRDResult.ReadNextSegmentFull();   
+
+                while t_success_NextSegmentFull == 1
+                    [t_success_NextSegmentFull, t_segmentLevel, t_segmentParent, t_hitObj, t_hitFace, t_insideOf, t_status,...
+                    t_x, t_y, t_z, t_l, t_m, t_n, t_exr, t_exi, t_eyr, t_eyi, t_ezr, t_ezi, t_intensity, t_pathLength,...
+                    t_xybin, t_lmbin, t_xNorm, t_yNorm, t_zNorm, t_index, t_startingPhase, t_phaseOf, t_phaseAt] = ZRDResult.ReadNextSegmentFull(); 
+                    if t_success_NextSegmentFull == 1
+                        Beam(RowCounter,1) = BeamCounter;
+                        Segment(RowCounter,1) = SegmentCounter;
+                        SegmentLevel(RowCounter,1) = t_segmentLevel;
+                        SegmentParent(RowCounter,1) = t_segmentParent;
+                        hitObj(RowCounter,1) = t_hitObj;
+                        hitFace(RowCounter,1) = t_hitFace;
+                        insideOf(RowCounter,1) = t_insideOf;
+                        status{RowCounter,1} = string(t_status);
+                        x(RowCounter,1) = t_x;
+                        y(RowCounter,1) = t_y;
+                        z(RowCounter,1) = t_z;
+                        l(RowCounter,1) = t_l;     
+                        m(RowCounter,1) = t_m;     
+                        n(RowCounter,1) = t_n;  
+                        exr(RowCounter,1) = t_exr;
+                        exi(RowCounter,1) = t_exi;
+                        eyr(RowCounter,1) = t_eyr;
+                        eyi(RowCounter,1) = t_eyi;
+                        ezr(RowCounter,1) = t_ezr;
+                        ezi(RowCounter,1) = t_ezi;
+                        intensity(RowCounter,1) = t_intensity;
+                        pathLength(RowCounter,1) = t_pathLength;
+                        xybin(RowCounter,1) = t_xybin;
+                        lmbin(RowCounter,1) = t_lmbin;
+                        xNorm(RowCounter,1) = t_xNorm;
+                        yNorm(RowCounter,1) = t_yNorm;
+                        zNorm(RowCounter,1) = t_zNorm;
+                        index(RowCounter,1) = t_index;
+                        startingPhase(RowCounter,1) = t_startingPhase;
+                        phaseOf(RowCounter,1) = t_phaseOf;
+                        phaseAt(RowCounter,1) = t_phaseAt;
+
+                        SegmentCounter = SegmentCounter + 1;
+                        RowCounter = RowCounter + 1;    
+                    end            
+                end
+
+                % Read Next Beam
+                [success_NextResult, rayNumber, waveIndex, wlUM, numSegments] = ZRDResult.ReadNextResult();
+                BeamCounter = BeamCounter + 1;
+            end
+
+            RayTable = table(Beam, Segment, SegmentLevel, SegmentParent, hitObj, hitFace, insideOf, status,...
+                         x, y, z, l, m, n, exr, exi, eyr, eyi, ezr, ezi, intensity, pathLength,...
+                         xybin, lmbin, xNorm, yNorm, zNorm, index, startingPhase, phaseOf, phaseAt);
+
+            if option
+                if exist('RayData.xlsx')
+                    delete('RayData.xlsx');
+                end
+                writetable(RayTable, 'RayData.xlsx');
+            end
         end
         
         function makeVariable(obj,surfaceID, paraName)
